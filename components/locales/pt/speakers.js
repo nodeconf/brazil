53d6981819e2a689d384a515f4f1eadaf020d05a
Alan Hoffmeister <alanhoffmeister@gmail.com>
2016-04-28 14:49:30 -0300

    Adding Kat & Nelson

--- components/locales/pt/speakers.js
+++ components/locales/pt/speakers.js
@@ -30,8 +30,8 @@ export default [
       Stack JS Development with MEAN". Fanático por Mega Man. JS também!`
   },
   {
-    name: 'Christopher EnyTc',
-    slug: 'christopher-enytc',
+    name: 'Christopher Silva',
+    slug: 'christopher-silva',
     job: 'DEVELOPER EVANGELIST | PAGAR.ME',
     image: '/assets/img/3.png',
     social: {
@@ -42,5 +42,27 @@ export default [
       Amante de tecnologia desde criança e perfeccionista. Apaixonado pela
       comunidade open-source e evangelista de Node.js, Go, Elixir, Rust e
       MongoDB. Atualmente é Developer Evangelist no Pagar.me`
+  },
+  {
+    name: 'Nelson Frugeri',
+    slug: 'nelson-frugeri',
+    job: 'DEVELOPER | CargoBR',
+    image: '/assets/img/nelson_frugeri.png',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/nelson-f-jr-72297465',
+      twitter: 'https://twitter.com/neldevfull'
+    },
+    description: 'Desenvolvedor de Software apaixonado por transformar ideias em soluções reais.'
+  },
+  {
+    name: 'Kat Marchán',
+    slug: 'kat-marchan',
+    job: 'DEVELOPER | npm',
+    image: '/assets/img/kat_marchan.png',
+    social: {
+      github: 'https://github.com/zkat',
+      twitter: 'https://twitter.com/maybekatz'
+    },
+    description: 'Kat Marchán é uma programadora na equipe de CLI do npm. Ela gosta de linguagens de programacão, linguagens de pessoas, linguage inclusivo, e pão de queijo. Ela é porto-riquenha e não fala português muito bem, mas tenta ;)'
   }
 ]
