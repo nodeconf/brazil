94feb9d84ad67487780c71fdd34f573efc442a83
Bruno Sato <bruno.sato@live.com>
2016-04-28 20:12:01 +0100

    translate date to english

--- components/locales/en/page.js
+++ components/locales/en/page.js
@@ -2,7 +2,7 @@
 
 export default {
   read_in_locale: 'LEIA EM PORTUGUÊS',
-  date: '4 e 5 de July',
+  date: '4th and 5th July',
   location: 'São Paulo, Brazil',
   buy_tickets_now: 'BUY YOUR TICKETS NOW',
   buy_tickets: 'Buy Tickets!',
