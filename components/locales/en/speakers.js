700944f9b23ba8d9daccdd243d4ea2a5b98eb0cd
Bruno Sato <bruno.sato@live.com>
2016-04-29 11:01:22 +0100

    Translate to english

--- components/locales/en/speakers.js
+++ components/locales/en/speakers.js
@@ -42,5 +42,76 @@ export default [
         Technology lover since childhood and perfectionist. Passionate about open- source community and
         evangelist Node.js, Go, Elixir, Rust and MongoDB. It is currently Developer
         Evangelist in Pagar.me`
+  },
+  {
+    name: 'Nelson Frugeri',
+    slug: 'nelson-frugeri',
+    job: 'DEVELOPER | CargoBR',
+    image: '/assets/img/nelson_frugeri.png',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/nelson-f-jr-72297465',
+      twitter: 'https://twitter.com/neldevfull'
+    },
+    description: `
+     Nelson is student of Information Systems in the UNIFACEF in Franca and 
+     work remote your city São Joaquim da Barra to company CargoBR in São Paulo
+     Technology lover, entrepreneurship and exponential technologies,
+     believe in bright future in which men and machines lived in a singular universe.`
+  },
+  {
+    name: 'Kat Marchán',
+    slug: 'kat-marchan',
+    job: 'DEVELOPER | npm',
+    image: '/assets/img/kat_marchan.png',
+    social: {
+      github: 'https://github.com/zkat',
+      twitter: 'https://twitter.com/maybekatz'
+    },
+    description: `
+    Kat Marchán ia a programmer in the team CLI of npm
+    She enjoys programming languages, languages of people,
+    linguage inclusive, and cheese bread. It is Puerto Rican and
+    doesn't speak Portuguese very well, but try  ;)`
+  },
+  {
+    name: 'Damian Schenkelman',
+    slug: 'damian-schenkelman',
+    job: 'ENGINEER | Auth0',
+    image: '/assets/img/damian-schenkelman.png',
+    social: {
+      github: 'https://github.com/dschenkelman',
+      twitter: 'https://twitter.com/dschenkelman'
+    },
+    description: `
+    Damian is an engineer in Auth0 working to make the core application scalable and performative.
+    He is lover about learning JavaScript, their specialty is in the area of perfomance and
+    contribution to open source software.`
+  },
+  {
+    name: 'Emerson Macedo',
+    slug: 'emerson-macedo',
+    job: 'LÍDER TÉCNICO | Globo.com',
+    image: '/assets/img/emerson_macedo.png',
+    social: {
+      github: 'https://github.com/emerleite',
+      twitter: 'https://twitter.com/emerleite'
+    },
+    description: `
+    Emerson Macedo is a software developer from adolescence, 
+    working in the area since 1997. He has worked in insurance, banking, telecommunications and web portals.
+    He currently is technical leader in the Globo.com and operates projects in the area of videos for internet.`
+  },
+  {
+    name: 'Alberto Souza',
+    slug: 'alberto-souza',
+    job: 'DEVELOPER | We.js',
+    image: '/assets/img/alberto_souza.png',
+    social: {
+      github: 'https://github.com/albertosouza',
+      twitter: 'https://twitter.com/albertosouza_js'
+    },
+    description: `
+    Hi I'm Javascript and PHP programmer, I have over 9 years of experience with development
+    I created and maintain the framework We.js and like to use my time to create things that help improve people's lives.`
   }
 ]
